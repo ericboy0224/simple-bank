7960a50b75b9244a6d49ea03978376e7c94b7272
Eric Lin <ericboy0224@gmail.com>
2024-05-23 08:36:05 +0000

    feat: add transfer transaction without balance

---
+++ db/sqlc/store.go
@@ -0,0 +1,102 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	connPool *pgxpool.Pool
+}
+
+// NewStore creates a new Store
+func NewStore(connPool *pgxpool.Pool) *Store {
+	return &Store{
+		connPool: connPool,
+		Queries:  New(connPool),
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.connPool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
+
+// TransferTxParams contains the parameters of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx performs a money transfer form one account to other
+// It creates a transfer, add account entries, and update accounts' balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		log.Printf("Starting transfer transaction from account %d to account %d with amount %d", arg.FromAccountID, arg.ToAccountID, arg.Amount)
+
+		result.Transfer, err = store.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = store.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = store.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: implement balance
+
+		log.Printf("Completed transfer transaction from account %d to account %d with amount %d", arg.FromAccountID, arg.ToAccountID, arg.Amount)
+
+		return nil
+	})
+
+	return result, err
+}
