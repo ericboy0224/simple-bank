170f5fe1a38ea106ec79a2d5d7b5f33da4565d1b
Eric Lin <ericboy0224@gmail.com>
2024-05-28 23:03:57 +0000

    test: fix operator

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -95,7 +95,7 @@ func TestTransferTx(t *testing.T) {
 		diff2 := toAccount.Balance - account2.Balance
 		require.Equal(t, diff1, diff2)
 		require.True(t, diff1 > 0)
-		require.True(t, diff1&amount == 0) // 1 * amount, 2 * amount ...
+		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount ...
 
 		k := int(diff1 / amount)
 		require.True(t, k >= 1 && k <= n)
